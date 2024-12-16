8e9b135560c759b24a82c4c8d6b69182cd3429ff
sivov <a.sivov@cft.ru>
2024-12-16 20:28:44 +0700

    добавили PREVED1

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("PREVED");
+        System.out.println("PREVED1");
         System.out.println("123153");
-        System.out.println("kjfghdffghdg");
+
         System.out.println("POKA");
         System.out.println("9886575436");
-        System.out.println("9886543543524575436");
+        System.out.println("hgfdhg");
         System.out.println("bgfsdgfd");
+        System.out.println("bgfsjytre67dgfd");
     }
 }
