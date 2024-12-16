ddac1fb0a759ded285d22e539fc81f833534741d
sivov <a.sivov@cft.ru>
2024-12-16 20:18:52 +0700

    добавили POKA

--- main/src/Main.java
+++ main/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        System.out.println("POKA");
+        System.out.println("9886575436");
+        System.out.println("9886543543524575436");
+        System.out.println("bgfsdgfd");
     }
 }
