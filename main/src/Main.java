5bda61a2b82087a6912bb51d13496651b5d9f887
Sivov Aleksandr <a.sivov@cft.ru>
2025-11-10 20:25:52 +0700

    добавл птиц

--- main/src/Main.java
+++ main/src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Приложение для поиска животных");
         System.out.println("Приложение для поиска котят");
+        System.out.println("Приложение для поиска птиц");
     }
 }
